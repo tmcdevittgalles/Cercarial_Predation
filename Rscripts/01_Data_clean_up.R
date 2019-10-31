574136e0b79352710fa5982f1ca824b66fe7204a
Travis McDevitt-Galles <travismcdevitt-galles@rgnt1-34-238-dhcp.int.colorado.edu>
2019-10-31 14:17:16 -0600

    added improved r code

---
+++ Rscripts/01_Data_clean_up.R
@@ -0,0 +1,17 @@
+## The goal of this code is to explore the variation and size in cercarial
+## predators in ponds in the East Bay Area
+## 
+## Creation date : 10/31/2010
+## 
+
+## load in necessary libraries
+library(tidyverse)
+library(lme4)
+
+
+# Set working directory
+setwd( "~/Desktop/Current_Projects/Cercarial_Predation" )
+
+
+# load in data
+field.df <- read.csv("./Data/Raw_Data/Cercarial_Predators.csv")
